a4a3fe360972d0f27abd2f9c196042f7f3a35361
Aynur <ainur345@yandex.ru>
2018-04-09 21:13:13 +0500

    Первый коммит

---
+++ addressbook_tests_white/addressbook_tests_white/Tests/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace addressbook_tests_white
+{
+    public class TestBase
+    {
+        public ApplicationManager app;
+
+        [SetUpFixture]
+        public void InitApplication()
+        {
+            app
+            app = new ApplicationManager();
+        }
+
+        [OneTimeTearDown]
+        public void StopApplication()
+        {
+            app.Stop();
+        }
+    }
+}
---
+++ addressbook_tests_white/addressbook_tests_white/Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace addressbook_tests_white
+{
+    [TestFixture]
+    public class UnitTest1 : TestBase
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
